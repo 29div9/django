7d7ad6d3b93f52fb5a37a583db1df66ca57d319c
Divya Kumari <29div94@gmail.com>
2020-09-22 09:24:14 +0530

    Added templates

--- trackmytasks/tasks/views.py
+++ trackmytasks/tasks/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+tasks = [
+    {
+        'title':'First task',
+        'assignee':'Divya Kumari',
+        'reported_on':'September 21, 2020',
+        'summary':'First task on portal'
+    },
+    {
+        'title': 'Second task',
+        'assignee': 'Jane Doe',
+        'reported_on': 'September 26, 2020',
+        'summary': 'Second task on portal'
+    },
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Welcome to trackmytasks!</h1>")
+    context = {
+        'tasks':tasks
+    }
+    return render(request,'tasks/home.html',context)
 
 
 def about(request):
-    return HttpResponse("<h1>About page</h1>")
+    return render(request,'tasks/about.html', {'title':'About'})
