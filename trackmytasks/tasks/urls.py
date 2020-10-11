8427466ba34325a94544528173de0f0927cf49e8
Divya Kumari <29div94@gmail.com>
2020-10-11 13:56:56 +0530

    tasks -list, details, create, update, delete

--- trackmytasks/tasks/urls.py
+++ trackmytasks/tasks/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
+from .views import (TaskListView,
+                    TaskDetailView,
+                    TaskCreateView,
+                    TaskUpdateView,
+                    TaskDeleteView)
 
 
 urlpatterns = [
-    path('', views.home, name="tasks-home"),
+    path('', TaskListView.as_view(), name="tasks-home"),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name="tasks-detail"),
+    path('tasks/new/', TaskCreateView.as_view(), name="tasks-create"),
+    path('tasks/<int:pk>/update/', TaskUpdateView.as_view(), name="tasks-update"),
+    path('tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name="tasks-delete"),
     path('about/', views.about, name="tasks-about"),
 ]
--- trackmytasks/tasks/views.py
+++ trackmytasks/tasks/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Task
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 
 
 def home(request):
@@ -8,6 +14,52 @@ def home(request):
         'tasks':Task.objects.all()
     }
     return render(request,'tasks/home.html',context)
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/home.html'       #<app_name>/<model>_<view>.html
+    context_object_name = 'tasks'
+    ordering = ['-reported_on']
+
+
+class TaskDetailView(DetailView):
+    model = Task
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = ['title', 'summary']
+
+    def form_valid(self, form):
+        form.instance.assignee = self.request.user
+        return super().form_valid(form)
+
+
+class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Task
+    fields = ['title', 'summary']
+
+    def form_valid(self, form):
+        form.instance.assignee = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        task = self.get_object()
+        if self.request.user == task.assignee:
+            return True
+        return False
+
+
+class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Task
+    success_url = '/'
+
+    def test_func(self):
+        task = self.get_object()
+        if self.request.user == task.assignee:
+            return True
+        return False
 
 
 def about(request):
